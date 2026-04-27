d42cce3184acdfa5c4112c6d0eb5a20fef4899bc
Marius-Florin Cristian <marius@keibisoft.com>
2026-04-27 18:22:00 +0300

    Update to v0.1.1

--- Formula/keibidrop.rb
+++ Formula/keibidrop.rb
@@ -3,23 +3,23 @@ class Keibidrop < Formula
   homepage "https://github.com/KeibiSoft/KeibiDrop"
   license "MPL-2.0"
 
-  version "0.1.0"
+  version "0.1.1"
 
   depends_on "macfuse" => :recommended
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/KeibiSoft/KeibiDrop/releases/download/v#{version}/keibidrop-#{version}-darwin-arm64.tar.gz"
-      sha256 "0fdd0e6578466f52819b340cd2ca82f31c22dfefa19a6ef101658b151d762870"
+      sha256 "9e28c357e6a1eb1b5ba6664a83674752b84b57a114352782c4b80e2dd16b96e7"
     else
       url "https://github.com/KeibiSoft/KeibiDrop/releases/download/v#{version}/keibidrop-#{version}-darwin-amd64.tar.gz"
-      sha256 "0d8b7acce887f9f616ff167a7d73e1b4ac083eb48bbba2ecbc87320e4b739f3c"
+      sha256 "5504c81a25f78f5c2aa6a6ee92351de1d0e43622cd9b67ac9a1d6bcc4d01918e"
     end
   end
 
   on_linux do
     url "https://github.com/KeibiSoft/KeibiDrop/releases/download/v#{version}/keibidrop-#{version}-linux-amd64.tar.gz"
-    sha256 "0433046d64efc73ea95249387456cda731dae61061eec694f51defb7bb646c04"
+    sha256 "0caf4aa7e20f4ae4076d24570d30b91a0fe1e1d64c7259158808d520c740b892"
     depends_on "libfuse2" => :recommended
   end
 
